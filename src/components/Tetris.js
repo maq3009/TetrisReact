5d7199082fac99fc5057a317ad0026f598ac96bc
Misael <maq3009@hotmail.com>
2024-02-26 03:58:13 -0400

    second commit

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,22 +3,29 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers';
+import { StyledTetris } from '../components/styles/StyledTetris';
+import { StyledTetrisWrapper } from '../components/styles/StyledTetris';
+
+
+
 
 
 const Tetris = () => {
 
     return(
-        <div>
-            <Stage stage={createStage()} />
-            <aside>
-                <div>
-                    <Display text="Score"/>
-                    <Display text="Rows"/>
-                    <Display text="Level"/>
-                </div>
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score"/>
+                        <Display text="Rows"/>
+                        <Display text="Level"/>
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
